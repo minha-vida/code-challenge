d2ea3bfc5bfe9ac312e2916ae59185c00de4a0c7
Renata Oliveira <renatabels@gmail.com>
2018-07-08 16:42:45 -0300

    :sparkles: Fetch Person

--- client/src/persons/PersonDetails.js
+++ client/src/persons/PersonDetails.js
@@ -1,95 +1,48 @@
 import React, { Component } from 'react'
+import PersonVaccines from './PersonVaccines';
 
 class PersonDetails extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      vaccines: [],
-      vaccine: {
-        name: '',
-        appliedAt: ''
-      }
+      person: {}
     }
   }
 
-  handleChangeVaccineName(event) {
-    this.setState({
-      vaccine: {
-        ...this.state.vaccine,
-        name: event.target.value
-      }
-    })
-  }
-
-  handleChangeVaccineAppliedAt(event, index) {
-    this.setState({
-      vaccine: {
-        ...this.state.vaccine,
-        appliedAt: event.target.value
-      }
-    })
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    console.log(this.state.vaccine)
-    this.setState({
-      vaccine: {
-        name: '',
-        appliedAt: ''
-      }
-    })
+  componentDidMount() {
+    const id = this.props.match.params.id
+    console.log('this.props:', this.props)
+    console.log('id', id)
+    fetch(`http://localhost:5000/persons/${id}`)
+      .then(response => response.json())
+      .then(person => {
+        this.setState({
+          person
+        })
+      })
+      .catch(err => console.error(err))
   }
 
   render() {
+    console.log(this.state.person)
     return (
       <div>
         <h1 className="mt-5 mb-2">Person Details</h1>
         <hr />
         <div className="row">
           <div className="col-lg-12 mb-2 justify-content-center text-center">
-            <img className="rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjCI42_N5Q7XKJBS9T3s-XtBGtTqTVriYhkFoIfmjOBkrQBmqt" width="100px" height="100px" />
+            <img className="rounded-circle" src={this.state.person.photo} width="100px" height="100px" />
           </div>
           <div className="col-lg-10">
             <label htmlFor="personName">Name</label>
-            <p className="form-control">Name</p>
+            <p className="form-control">{this.state.person.name}</p>
           </div>
           <div className="col-lg-2">
             <label htmlFor="personAge">Age</label>
-            <p className="form-control">Age</p>
+            <p className="form-control">{this.state.person.age}</p>
           </div>
         </div>
-        <div className="row mt-4 mb-2">
-          <h2 className="ml-3">Vaccines</h2>
-        </div>
-        <div>
-          <div className="mt-2">
-            <div className="row mt-2">
-              <div className="col-sm-8">
-                <label htmlFor="vaccineName">Name</label>
-                <input onChange={(e) => this.handleChangeVaccineName(e)} value={this.state.vaccine.name} type="text" className="form-control" placeholder="Vaccine Name" />
-              </div>
-              <div className="col-sm-4">
-                <label htmlFor="dependentDocumentNumber">AppliedAt</label>
-                <input onChange={(e) => this.handleChangeVaccineAppliedAt(e)} value={this.state.vaccine.appliedAt} type="text" className="form-control" placeholder="AppliedAt" />
-              </div>
-            </div>
-          </div>
-          < div className="row mb-4 mr-1 mt-4 float-right" >
-            <button onClick={this.handleSubmit.bind(this)} type="button" className="btn btn-primary mr-2">Save</button>
-          </div>
-          <div className="card mb-2" style={{ width: '100%' }}>
-            <div className="card-body">
-              <h5 className="card-title">Rubeola</h5>
-              <hr />
-              <h6 className="card-subtitle mb-2 text-muted">AppliedAt: 12/05/2018</h6>
-              <p className="card-text">CreatedAt: 12/05/2018</p>
-              <p className="card-text">UpdatedAt: 12/05/2018</p>
-              <a href="#" class="card-link">Edit</a>
-              <a href="#" class="card-link">Delete</a>
-            </div>
-          </div>
-        </div>
+        {this.state.person.id && <PersonVaccines personId={this.state.person.id} />}
       </div>
     )
   }
