9e1f58841201c7761c67828701fc5c40a146a977
Renata Oliveira <renatabels@gmail.com>
2018-07-08 21:29:17 -0300

    :construction: Input date format placeholder

--- client/src/persons/RegisterPersonVaccine.js
+++ client/src/persons/RegisterPersonVaccine.js
@@ -64,7 +64,7 @@ class RegisterPersonVaccine extends Component {
             </div>
             <div className="col-sm-4">
               <label htmlFor="dependentDocumentNumber">AppliedAt</label>
-              <input onChange={(e) => this.handleChangeVaccineAppliedAt(e)} value={this.state.vaccine.appliedAt} type="text" className="form-control" placeholder="AppliedAt" />
+              <input onChange={(e) => this.handleChangeVaccineAppliedAt(e)} value={this.state.vaccine.appliedAt} type="text" className="form-control" placeholder="YYYY-MM-DD" />
             </div>
           </div>
         </div>
