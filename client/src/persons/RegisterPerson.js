f6aab393adf051b107fb61ffadd5d192a2531be0
Renata Oliveira <renatabels@gmail.com>
2018-07-09 01:06:32 -0300

    :sparkles: Encode Person Photo

--- client/src/persons/RegisterPerson.js
+++ client/src/persons/RegisterPerson.js
@@ -1,68 +1,107 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router'
 
 class RegisterPerson extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
-      age: '',
-      photo: ''
+      person: {
+        name: '',
+        age: '',
+        photo: ''
+      },
+      registered: false
     }
   }
 
   handleNameChange(event) {
     this.setState({
-      name: event.target.value
+      person: {
+        ...this.state.person,
+        name: event.target.value
+      }
     })
   }
 
   handleAgeChange(event) {
     this.setState({
-      age: event.target.value
+      person: {
+        ...this.state.person,
+        age: event.target.value
+      }
     })
   }
 
   handlePhotoChange(event) {
+    var self = this;
+    var reader = new FileReader();
+    var file = event.target.files[0];
+
+    reader.onload = function (e) {
+      self.setState({
+        person: {
+          ...self.state.person,
+          photo: e.target.result
+        }
+      });
+      console.log(self.state.photo)
+    };
+    reader.readAsDataURL(file)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    fetch(`http://localhost:5000/persons`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(this.state.person)
+    })
+      .then(response => response.json())
+      .then(registered =>
+        this.setState({
+          registered: true
+        }))
+      .catch(error => console.error(`Fetch Error =\n`, error))
+
     this.setState({
-      photo: event.target.value
+      person: {
+        name: '',
+        age: '',
+        photo: ''
+      }
     })
   }
 
   render() {
     return (
       <div>
+        {this.state.registered && <Redirect to="/persons" />}
         <div className="container">
           <h1 className="mt-5 mb-2">Register Person</h1>
           <hr />
-          <form className="needs-validation" novalidate>
+          <form className="needs-validation" onSubmit={this.handleSubmit.bind(this)}>
             <div className="form-row">
               <div className="col-md-6 mb-3">
-                <label for="validationTooltip01">Name</label>
-                <input type="text" className="form-control" id="validationTooltip01" placeholder="Name" value="" required />
-                <div className="valid-tooltip">
-                  Looks good!
-                </div>
+                <label htmlFor="validationTooltip01">Name</label>
+                <input onChange={(e) => this.handleNameChange(e)} value={this.state.person.name} type="text" className="form-control" id="validationTooltip01" placeholder="Name" required />
               </div>
               <div className="col-md-2 mb-3">
-                <label for="validationTooltip02">Age</label>
-                <input type="text" className="form-control" id="validationTooltip02" placeholder="Age" value="" required />
-                <div className="valid-tooltip">
-                  Looks good!
-                </div>
+                <label htmlFor="validationTooltip02">Age</label>
+                <input onChange={(e) => this.handleAgeChange(e)} value={this.state.person.age} type="text" className="form-control" id="validationTooltip02" placeholder="Age" required />
               </div>
               <div className="col-md-4 mb-3">
-                <label for="validationTooltipUsername">Photo</label>
+                <label htmlFor="validationTooltipUsername">Photo</label>
                 <div className="input-group">
                   <div className="custom-file">
-                    <input type="file" className="custom-file-input" id="inputGroupFile01" />
-                    <label className="custom-file-label" for="inputGroupFile01">Choose file</label>
+                    <input onChange={(e) => this.handlePhotoChange(e)} type="file" className="custom-file-input" id="inputGroupFile01" />
+                    <label className="custom-file-label" htmlFor="inputGroupFile01">Choose file</label>
                   </div>
-
                 </div>
-                <div className="invalid-tooltip">
-                  Please choose a photo for the person.
-                  </div>
               </div>
             </div>
             <div className="float-right" >
