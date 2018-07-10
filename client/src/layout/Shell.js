a151b6c3b4c673cb2256e1d8e7890cff689817f3
Renata Oliveira <renatabels@gmail.com>
2018-07-10 13:02:52 -0300

    :bug: Fixing NavBar Collapse

--- client/src/layout/Shell.js
+++ client/src/layout/Shell.js
@@ -13,26 +13,28 @@ class Shell extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
             <Link to='/' className="navbar-brand">Vaccine</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
             </button>
-            {this.props.user &&
-              (<div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="/persons" role="button" aria-haspopup="true" aria-expanded="false">Persons</a>
-                    <div className="dropdown-menu">
-                      <Link to={'/persons'} className="dropdown-item">List Persons</Link>
-                      <Link to={'/persons/new'} className="dropdown-item">Register Person</Link>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-              )}
-            <UserProfile />
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="/persons" role="button" aria-haspopup="true" aria-expanded="false">Persons</a>
+                  <div className="dropdown-menu">
+                    <Link to={'/persons'} className="dropdown-item">List Persons</Link>
+                    <Link to={'/persons/new'} className="dropdown-item">Register Person</Link>
+                  </div>
+                </li>
+              </ul>
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <UserProfile />
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
         <div className="container">
