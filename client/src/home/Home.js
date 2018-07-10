cfee45a9392246523d714cfed93f24208c977e88
Renata Oliveira <renatabels@gmail.com>
2018-07-10 05:05:33 -0300

    :Adding Home Page with Readme.md

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import marked from 'marked'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { readme: '' }
+  }
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/rnataoliveira/code-challenge/master/README.md')
+      .then(response => response.text())
+      .then(readme => {
+        this.setState({
+          readme: marked(readme)
+        })
+      })
+  }
 
   render() {
     return (
-      <div>
-        <h1>This is my home</h1>
-      </div>
+      <article className="mt-2" dangerouslySetInnerHTML={{__html: this.state.readme}}></article>
     )
   }
 }
